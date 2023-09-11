cd1381a2355579b5c9e25c91541011eaa84ae38d
Ilia Frenkel <frenkel.ilia@gmail.com>
2023-09-11 17:01:43 +1000

    Change comments style for backward compatibiliy (C89)

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
-// Copyright 2023 Ilia Frenkel. All rights reserved.
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE.txt file.
+/*
+ * Copyright 2023 Ilia Frenkel. All rights reserved.
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE.txt file.
+ */
 
 #include <stdio.h>
 
