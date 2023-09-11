57766af4df897322cc216b6ceeaa1dd0bd298393
Ilia Frenkel <frenkel.ilia@gmail.com>
2023-09-11 23:26:12 +1000

    Add a greeting function

--- hello.c
+++ hello.c
@@ -1,18 +1,21 @@
-/*
- * Copyright 2023 Ilia Frenkel. All rights reserved.
- * Use of this source code is governed by a MIT-style
- * license that can be found in the LICENSE.txt file.
- */
+/* Copyright 2023 Ilia Frenkel. All rights reserved.
+   Use of this source code is governed by a MIT-style
+   license that can be found in the LICENSE.txt file. */
 
 #include <stdio.h>
 
-/***********************************
+/*
+ * Returns a greeting as a string.
+ */
+char *greeting() { return "Hello, World!\n"; }
+
+/*
+ * Prints a greeting and returns.
  * Function : int main(void)
  * Input    : none
  * Output   : Returns 0 on success
- * Prints "Hello, World!" greeting.
- **********************************/
+ */
 int main(void) {
-    printf("Hello, World!\n");
+    printf("%s", greeting());
     return 0;
 }
