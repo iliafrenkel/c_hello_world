621b58ccc11880df4d270049e5097c6949823273
Ilia Frenkel <frenkel.ilia@gmail.com>
2023-09-20 17:17:48 +1000

    Fix the comments

--- hello.c
+++ hello.c
@@ -11,7 +11,7 @@
 #define MAX_NAME_SIZE 50
 
 /*
- * Gets a string from stdin and returns it with '\n' removed.
+ * Gets a string from stdin and returns without the trailing '\n'.
  * Function : void get_name(char *pszName, int buffSize)
  * Input:
  *     - char *pszName, a buffer to put the user input in
@@ -24,21 +24,20 @@ void get_name(char *pszName, size_t buffSize) {
     }
     /*
      * Use `fgets` to get the user input because `gets` and `scanf` are
-     * considered unsafe because they don't check for the length of the
-     * input. Really?!
+     * considered unsafe. They don't check for the length of the input!
      */
     if (fgets(pszName, buffSize, stdin) == NULL) {
         printf("\rCouldn't read the user input: ");
-        if (feof(stdin) != 0) {
+        if (feof(stdin) != 0) { // this happens if you press Ctrl+D
             printf("unexpected end of file.\n");
-        } else if (ferror(stdin) != 0) {
+        } else if (ferror(stdin) != 0) { // I don't know what can cause this
             printf("error while reading from a file.\n");
         }
         clearerr(stdin);
         return;
     }
     /* Remove the '\n' at the end */
-    const unsigned int len = strlen(pszName); // TODO: what does 'const' do?
+    const unsigned int len = strlen(pszName);
     if (len > 0) {
         pszName[len - 1] = '\0';
     }
@@ -65,8 +64,8 @@ int main(int argc, char *argv[]) {
     }
 
     /*
-     * User our greeter API to greet the user.
-     * This is the main point if the current version.
+     * Use our greeter API to greet the user.
+     * This is the main point of the current version.
      * Everything else up to this point was just fluff.
      */
     greeter_api.say_hello(name);
